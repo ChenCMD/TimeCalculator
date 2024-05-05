35c7cff62586d086dcbbde9f27eaa80840f5aac4
ChenCMD <scnmed.g@gmail.com>
2024-05-06 05:13:34 +0900

    :lipstick: レイアウト調整

--- src/main/scala/com/github/chencmd/timecalculator/TimeCalculator.scala
+++ src/main/scala/com/github/chencmd/timecalculator/TimeCalculator.scala
@@ -18,8 +18,8 @@ def main(): Unit = {
 def TimeCalculator() = {
   val expr   = Var("2:22 + 1d2h3m - (9h03m - 3h)")
   val format = Var(ResultFormat.HM)
+
   mainTag(
-    margin := "0 1em",
     h1(
       "Time Calculator",
       a(
@@ -30,20 +30,25 @@ def TimeCalculator() = {
     div(
       p("時刻の加減算を行う自分用の雑なツールだよ"),
       p("時刻の形式は以下のようなものが使えるよ"),
-      table(
-        {
-          def styledTD(s: String) = td(s, textAlign := "end", padding := "0em 1em", border := "solid black", borderWidth := "0 1px")
-          val examples            = List(
-            "1:15"     -> "1 時間 15 分",
-            "1:02:3"   -> "1 時間 2 分 3 秒",
-            "1d"       -> "1 日",
-            "2d5h3s"   -> "2 日 5 時間 3 秒",
-            "1d2h3m4s" -> "1 日 2 時間 3 分 4 秒"
-          )
-          examples.map { case (input, desc) => tr(styledTD(input), styledTD(desc)) }
-        }*
-      ),
-      margin := "2em 0",
+      table {
+        def styledTD(text: String) = td(
+          text,
+          textAlign   := "end",
+          padding     := "0em 1em",
+          border      := "solid black",
+          borderWidth := "0 1px"
+        )
+
+        val examples = List(
+          "1:15"     -> "1 時間 15 分",
+          "1:02:3"   -> "1 時間 2 分 3 秒",
+          "1d"       -> "1 日",
+          "2d5h3s"   -> "2 日 5 時間 3 秒",
+          "1d2h3m4s" -> "1 日 2 時間 3 分 4 秒"
+        )
+        examples.map { case (input, desc) => tr(styledTD(input), styledTD(desc)) }
+      },
+      margin  := "2.5em 0"
     ),
     div(
       input(
@@ -51,8 +56,9 @@ def TimeCalculator() = {
         size <-- expr.signal.map(s => Math.max(s.length - 2, 10)),
         controlled(
           value <-- expr.signal,
-          onInput.mapToValue.filter("^[+-=0-9:smhd ]*$".r.matches) --> expr.writer
-        )
+          onInput.mapToValue.filter("^[+-=0-9:smhd() ]*$".r.matches) --> expr.writer
+        ),
+        fontSize        := "1em"
       ),
       span("=", padding := "0 0.5em"),
       select(
@@ -63,20 +69,27 @@ def TimeCalculator() = {
             selected <-- format.signal.map(_ == fmt)
           )
         },
-        onChange.mapToValue.map(s => ResultFormat.fromOrdinal(s.toInt)) --> format.writer
+        onChange.mapToValue.map(s => ResultFormat.fromOrdinal(s.toInt)) --> format.writer,
+        fontSize        := "1em"
       )
     ),
     div(
-      child.text <-- {
+      child <-- {
         for {
           rawExpr <- expr.signal
           format  <- format.signal
         } yield ExpressionParser.parse(rawExpr) match {
-          case Right(expr) => s"$rawExpr = ${format.use(expr.eval)}"
+          case Right(expr) => div(
+              span(s"$rawExpr", fontSize          := "1em"),
+              span("=", padding                   := "0 0.5em"),
+              span(format.use(expr.eval), padding := "0 0.5em", border := "black double", borderWidth := "0 0 5px 0")
+            )
           case Left(err)   => s"err: $err"
         }
       },
       padding := "1em 0"
-    )
+    ),
+    margin := "0 1em",
+    fontSize := "1.3em"
   )
 }
